3fa5dc7b297ffe80ba39c27f1d64806e9ca683a5
Mehmet <mehmetberber84@gmail.com>
2022-06-03 15:01:39 +0300

    Initial Commit

---
+++ BottomTabbar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BottomTabbar
+//
+//  Created by Erkam on 3.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
